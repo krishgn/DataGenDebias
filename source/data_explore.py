4c423bef682baed5e724fe7acb9fcf7ad5f2a511
krishgn <krish.kgn@gmail.com>
2020-01-31 15:55:40 -0800

    Adding the .py files for adult data set analysis.

--- source/data_explore.py
+++ source/data_explore.py
@@ -2,15 +2,28 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
-from data_operations import svm_create_accuracy
+from data_operations import sv_accuracy
 from data_operations import create_test_train
-from data_preprocess import create_jobid
 from sklearn import preprocessing
-from sklearn import utils
+from collections import Counter
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier, VotingClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, learning_curve, train_test_split, KFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+sns.set(style='white', context='notebook', palette='deep')
 sns.set()
 
 #Just to get some basic info
-data_orig = pd.read_csv('../data/SF_salary_data_gender.csv',low_memory=False)
+data_orig = pd.read_csv('../data/adult.csv',low_memory=False)
 print("\n \n The original columns of the input file are: \n")
 print(data_orig.info())
 print("\n \n The original data set looks like this: \n")
@@ -18,61 +31,78 @@ print(data_orig.head())
 print("\n \n The size of the data set is \n")
 print(data_orig.shape)
 
-#Let's check the year wise salary distribution
-data_orig[["Year","TotalPay"]].groupby("Year").mean()
-data_orig['Year'].plot(kind='hist')
-plt.show()
+#Identify the numerical and categorical features
+num_feat = ['age','fnlwgt','educational-num','capital-gain','capital-loss','hours-per-week','income']
 
-#That's a lot of data and a lot of data per year. So let's just work on the 2014 salaries
-data_14 = data_orig[(data_orig['Year'] == 2014)]
-data_14.drop(labels=["Year"], axis = 1, inplace = True)
-print("\n \n The new columns of the input file are: \n")
-print(data_14.info())
-print("\n \n The size of the new data set is \n")
-print(data_14.shape)
+# Identify Categorical features
+cat_feat = ['workclass','education','marital-status', 'occupation', 'relationship', 'race', 'sex', 'native']
 
+#Fill in the missing values with the mode value of the columns
+for col in ['workclass', 'occupation', 'native-country']:
+    data_orig[col].fillna(data_orig[col].mode()[0], inplace=True)
+
+# Count of >50K & <=50K
+sns.countplot(data_orig['income'],label="Count")
+#plt.show()
+
+# Correlation matrix between numerical values
+fig_heatmap = sns.heatmap(data_orig[num_feat].corr(),annot=True, fmt = ".2f", cmap = "coolwarm")
+#plt.show()
+
+
+# map the income prediction to 0 & 1
+data_orig['income']=data_orig['income'].map({'<=50K': 0, '>50K': 1, '<=50K.': 0, '>50K.': 1})
 #Now to compare the gender difference:
-fig_gender = sns.barplot(x="gender",y="TotalPay",data=data_14)
-fig_gender = fig_gender.set_ylabel("Income")
-plt.show()
+fig_gender = sns.barplot(x="gender",y="income",data=data_orig)
+fig_gender = fig_gender.set_ylabel("Income >50K Probability")
+#plt.show()
 
-#Cleaning the data to keep just the essential features
-data_input = data_14.filter(['JobTitle','TotalPay','gender'])
-print('Input Data Labels')
-print(data_input.head())
+#Feature engineering
+data_orig["gender"] = data_orig["gender"].map({"Male": 0, "Female":1})
 
-#Taking a look at the job_titles
-data_input["JobTitle"].value_counts().head(10)
-#Now to convert the jobtitle strings to numbers and drop the JobTitle column
-JobTitle_enc = preprocessing.LabelEncoder()
-JobTitle_enc.fit(data_input["JobTitle"] )
-JobTitle_Lowercase = JobTitle_enc.transform(data_input["JobTitle"] )
-print(type(JobTitle_Lowercase))
-data_input.insert(0, "JobId", JobTitle_Lowercase.astype(int), True)
-data_input.drop(labels=["JobTitle"], axis = 1, inplace = True)
-print(data_input.head())
+# Create Married Column - Binary Yes(1) or No(0)
+data_orig["marital-status"] = data_orig["marital-status"].replace(['Never-married','Divorced','Separated','Widowed'], 'Single')
+data_orig["marital-status"] = data_orig["marital-status"].replace(['Married-civ-spouse','Married-spouse-absent','Married-AF-spouse'], 'Married')
+data_orig["marital-status"] = data_orig["marital-status"].map({"Married":1, "Single":0})
+data_orig["marital-status"] = data_orig["marital-status"].astype(int)
 
-#Mapping the gender info to 0&1
-data_input["gender"] = data_input["gender"].map({"m": 1, "f":0})
+#Create race column - binary White-1 and nonwhite-0
+print(data_orig.race.unique())
+data_orig["race"]= data_orig["race"].replace(['Black','Asian-Pac-Islander','Other','Amer-Indian-Eskimo'],'Minorities')
+data_orig["race"]=data_orig["race"].map({"White":1,"Minorities":0})
+data_orig["race"] = data_orig["race"].astype(int)
+
+#Drop the data you don't want to use
+data_orig.drop(labels=["workclass","education","occupation","relationship","native-country"], axis = 1, inplace = True)
+print('Dataset with Dropped Labels')
+print(data_orig.head())
+print(data_orig.info())
 
 #create two datasets, male and female and create the job ids
-data_male = data_input[(data_input['gender'] == 1)]
-data_female = data_input[(data_input['gender'] == 0)]
+data_male = data_orig[(data_orig['gender'] == 1)]
+data_female = data_orig[(data_orig['gender'] == 0)]
 
 #Create test and train data based on target column TotalPay
-target_name = "TotalPay"
+target_name = "income"
 
+
+# X_train, X_test, Y_train, Y_test = create_test_train(data_orig,data_orig)
 X_train_m, X_test_m, Y_train_m, Y_test_m = create_test_train(data_male,target_name)
 X_train_f, X_test_f, Y_train_f, Y_test_f = create_test_train(data_female,target_name)
 
-acc_svc_mr_ms = svm_create_accuracy(X_train_m,Y_train_m,X_test_m,Y_test_m)
+acc_svc_mr_ms, pred = sv_accuracy(X_train_m,Y_train_m,X_test_m,Y_test_m)
 print("\n Accuracy with male train and male test data: ",round(acc_svc_mr_ms,2),"%")
+print(confusion_matrix(Y_test_m, pred))
 
-acc_svc_mr_fs = svm_create_accuracy(X_train_m,Y_train_m,X_test_f,Y_test_f)
+
+acc_svc_mr_fs, pred = sv_accuracy(X_train_m,Y_train_m,X_test_f,Y_test_f)
 print("\n Accuracy with male train and female test data: ",round(acc_svc_mr_fs,2),"%")
+print(confusion_matrix(Y_test_f, pred))
 
-acc_svc_fr_fs = svm_create_accuracy(X_train_f,Y_train_f,X_test_f,Y_test_f)
+acc_svc_fr_fs, pred = sv_accuracy(X_train_f,Y_train_f,X_test_f,Y_test_f)
 print("\n Accuracy with female train and female test data: ",round(acc_svc_fr_fs,2),"%")
+print(confusion_matrix(Y_test_f, pred))
 
-acc_svc_fr_ms = svm_create_accuracy(X_train_f,Y_train_f,X_test_m,Y_test_m)
+acc_svc_fr_ms, pred = sv_accuracy(X_train_f,Y_train_f,X_test_m,Y_test_m)
 print("\n Accuracy with female train and male test data: ",round(acc_svc_fr_ms,2),"%")
+print(confusion_matrix(Y_test_m, pred))
