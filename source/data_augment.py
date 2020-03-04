5444df403e30dc5020f0eaf4edc3ca681e3210d2
krishgn <krish.kgn@gmail.com>
2020-03-03 17:58:23 -0800

    script to augment the original data with synthetic data

--- source/data_augment.py
+++ source/data_augment.py
@@ -6,21 +6,24 @@ Augmentation Factor = 1 is when no of males = no of females
 import pandas as pd
 from sklearn.utils import shuffle
 
+def number_of_synthetic_data(aug_fact, data):
+    """
+    This function finds the number of data points to be extracted
+    from synthetic data that needs to be augmented to original data
+    """
+    nfemales, nmales = data.groupby('sex').size()
+    ndiff = nmales/nfemales - 1 #factor of difference in male and female nos
+    return round(ndiff * aug_fact * nfemales)
 
+#input augmentation factor
 aug_fact = 0.25
-
 data_orig = pd.read_csv('../data/processed_adult.csv',low_memory=False)
-datagen_female  = pd.read_csv('../data/generated_female_15k.csv',low_memory=False)
+datagen_female  = shuffle(pd.read_csv('../data/generated_female_15k.csv',low_memory=False))
 
-nfemales, nmales = data_orig.groupby('sex').size()
+augmentation_num = number_of_synthetic_data(aug_fact, data_orig)
+#from the aug_fact add the requisite number of data points from synthetic data to the original data
+data_augmented  = shuffle(data_orig.append(datagen_female[:augmentation_num], sort = False))
 
-ndiff = nmales/nfemales - 1
-
-ndata = ndiff * aug_fact
-
-datagen_female = shuffle(datagen_female)
-data_new = datagen_female[:round(ndata)]
-
-data_augmented  = data_orig.append(data_new,sort = False)
-
-data_augmented = shuffle(data_orig)
+#write the augmented data into a new file for analysis
+new_file_name = "../data/augmented_data_" + str(aug_fact) + ".csv"
+data_augmented.to_csv(new_file_name,index=False)
